3b9cc6b9541e37073a2b01ee03fbdbf43223c264
mkaplanci <mkaplanci@havelsan.com.tr>
2018-12-10 18:26:45 +0300

    fsf

---
+++ ppmtool-react-client/src/store.js
@@ -0,0 +1,28 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const initialState = {};
+const middleware = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+    store = createStore(
+        rootReducer,
+        initialState,
+        composeWithDevTools(
+            applyMiddleware(...middleware)
+
+        )
+    );
+} else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(applyMiddleware(...middleware))
+    );
+}
+
+export default store;
